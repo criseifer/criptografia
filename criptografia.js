ffaf72430722e743b5a31c8d22b5c84948569636
cristiane Fensterseifer <criseifer@hotmail.com>
2024-08-26 19:27:40 -0300

    feat: add function to decypher

--- criptografia.js
+++ criptografia.js
@@ -1,12 +1,13 @@
-//FAZER A CRIPTOGRAFIA Criar a função
-//Ligar a função ao botão correspondente no HTML
-//Capturar o texto escrito no campo de input do HTML
-//Desenvolver a lógica da criptografia
-//retornar na tela o resultado do texto criptografado
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
 
+function btneEncriptar (){
+    const textCriptografado = criptoGrafar(textArea.value);
+    mensagem.value = textCriptografado
+    textArea.value = ""
+}
 
-    
-function criptoGrafar(){
+function criptoGrafar(textoDigitado){
 
     const mapaVogais = {
         "a": "ai",
@@ -15,67 +16,44 @@ function criptoGrafar(){
         "o": "ober",
         "u": "ufat"
     }
-
-    // document.querySelector('textarea')=""
-    var textoDigitado = document.querySelector('textarea').value
-    console.log(textoDigitado);
+    
     textoDigitado = textoDigitado.toLowerCase();
 
-    let textCriptogradafo = "";
-    console.log(textoDigitado.length)
+    let textCriptografado = "";
+
     for (i=0; i<textoDigitado.length; i++) {
-        console.log(i);
-        console.log(textoDigitado[i]);
-        console.log(textCriptogradafo);
+
         if (textoDigitado[i] in mapaVogais) {
-            textCriptogradafo = textCriptogradafo + mapaVogais[textoDigitado[i]];
+            textCriptografado = textCriptografado + mapaVogais[textoDigitado[i]];
         } else {
-            textCriptogradafo = textCriptogradafo + textoDigitado[i];
+            textCriptografado = textCriptografado + textoDigitado[i];
         }
+    };
+    return textCriptografado;
+}
+
+function btneDesencriptar (){
+    const textDescriptografado = descriptoGrafar(textArea.value);
+    mensagem.value = textDescriptografado
+    textArea.value = ""
+}
+
+function descriptoGrafar(textoDigitado){
+
+    const mapaVogais2 = {
+        "ai": "a",
+        "enter": "e",
+        "imes": "i",
+        "ober": "o",
+        "ufat": "u"
     }
+    textoDigitado = textoDigitado.toLowerCase();
 
-    console.log(textCriptogradafo);
+    for (const [key, value] of Object.entries(mapaVogais2)) {
+
+        textoDigitado = textoDigitado.replace(key, value)
+
+    };
+
+    return textoDigitado;
 }
-
-
-    
-    
-    
-
-
-
-function descriptoGrafar(){
-    console.log('o botao descriptografar foi clicado')
-}
-
-
-let letra
-
-const mapaVogais = {
-       "a": "ai",
-        "e": "enter",
-        "i": "imes",
-        "o": "ober",
-        "u": "ufat"
-}
-// //let textoCriptografado = "";
-
-// //if (letra == "a"){
-//   //  TextoDigitado.replace(letra,"ai");
-// //} else if (letra == "e"){
-   // TextoDigitado.replace(letra,"enter");
-//} else if (letra == "i"){
- //   TextoDigitado.replace(letra,"imes");
-//} else if (letra == "o"){
-  //  TextoDigitado.replace(letra,"ober");
-//} else if(letra=="u"){
-  //  TextoDigitado.replace(letra,"ufat")
-//}
-
-
-
-//FAZER A DESCRIPTOGRAFIA Criar a função
-//Ligar a função ao botão correspondente no HTML
-//Capturar o texto escrito no campo de input do HTML
-//Desenvolver a lógica da descriptografia
-//retornar na tela o resultado do texto descriptografado
